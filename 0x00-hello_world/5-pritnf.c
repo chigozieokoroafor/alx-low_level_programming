cb19fec288aca60d33a8416a84615ad8e262b6d2
chigozieokoroafor <okoroaforc14@gmail.com>
2022-06-30 14:59:18 -0700

    prints using printf

---
+++ 0x00-hello_world/5-pritnf.c
@@ -0,0 +1,11 @@
+/*
+ * main - Entry point
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s",str);
+	return (0);
+}
