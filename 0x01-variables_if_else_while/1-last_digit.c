7393cdf0a6f58ae49f9739b18546a2993209cce6
chigozieokoroafor <okoroaforc14@gmail.com>
2022-07-04 15:10:46 -0700

    digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -29,7 +29,7 @@ int main(void)
 	else if (last_digit < 6 || last_digit != 0)
 	{
 		printf("Last digit of %d ", n);
-		printf("is %d and is less than 6 and not 0", last_digit);
+		printf("is %d and is less than 6 and not 0\n", last_digit);
 	}
 	return (0);
 }
